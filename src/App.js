b7588d462f9a8c84f4cfbe8e27255af2b878fb5a
DFWTexan <ErishMF@Gmail.com>
2023-07-03 12:56:23 -0500

    - Layout Progress SAVE...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.scss';
-import 'boxicons/css/boxicons.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AppLayout from './components/layout/AppLayout';
-import Blank from './pages/Blank';
+import "./App.scss";
+import "boxicons/css/boxicons.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./components/layout/AppLayout";
+import Blank from "./pages/Blank";
+import Draftboard from "./pages/Draftboard";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<AppLayout />}>
-                    <Route index element={<Blank />} />
-                    <Route path='/fantasyteams' element={<Blank />} />
-                    <Route path='/setup' element={<Blank />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Draftboard />} />
+          <Route path="/fantasyteams" element={<Blank />} />
+          <Route path="/setup" element={<Blank />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Draftboard.jsx
@@ -0,0 +1,13 @@
+import '../styles/index.scss'
+
+const Draftboard = () => {
+  return (
+    <div className='container'>
+      <div className='displayCol'>This is LEFT</div>
+      <div className='displayCol'>This is CENTER</div>
+      <div className='displayCol'>This is RIGHT</div>
+    </div>
+  );
+};
+
+export default Draftboard;
