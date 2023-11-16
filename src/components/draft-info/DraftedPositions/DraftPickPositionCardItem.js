90e16b544a3be943a42c6d9e27ca9f22f8325663
DFWTexan <ErishMF@Gmail.com>
2023-11-16 11:18:02 -0600

    Update DraftPickPositionCardItem.js

--- src/components/draft-info/DraftedPositions/DraftPickPositionCardItem.js
+++ src/components/draft-info/DraftedPositions/DraftPickPositionCardItem.js
@@ -4,11 +4,8 @@ import "../../../styles/index.scss";
 
 const CardInfo = ({ draftPick }) => (
   <div className="d-flex justify-content-start">
-
-{console.log("==> EMFTest (DraftPickPositionCardItem) - draftPick: ", draftPick === "" ? "null" : draftPick)}
-
-
-    {draftPick.round + "." + draftPick.pickInRound}  {draftPick && ("   " + draftPick.playerName)}
+    {draftPick.round + "." + draftPick.pickInRound}{" "}
+    {draftPick && "   " + draftPick.playerName}
   </div>
 );
 
