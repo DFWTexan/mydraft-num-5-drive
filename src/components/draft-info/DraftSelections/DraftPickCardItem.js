65fb5389b35cec42bffee90c5f91255bd62edcc2
DFWTexan <ErishMF@Gmail.com>
2023-11-28 14:21:20 -0600

    Update DraftPickCardItem.js

--- src/components/draft-info/DraftSelections/DraftPickCardItem.js
+++ src/components/draft-info/DraftSelections/DraftPickCardItem.js
@@ -6,22 +6,24 @@ const CardInfo = ({ draftPick, otcID, currentPick }) => {
   const className = draftPick.isMyTeamPick
     ? "draft-selection-card-my-team-pick"
     : draftPick.teamID === otcID
-    ? draftPick.overallPick === currentPick 
-    ? "draft-selection-card-on-the-clock"
-    : "draft-selection-card"
+    ? draftPick.overallPick === currentPick
+      ? "draft-selection-card-on-the-clock"
+      : "draft-selection-card"
     : "draft-selection-card";
 
   return (
     <div className={className}>
       {draftPick.round + "." + draftPick.pickInRound}{" "}
       <div className="draft-selection-card__title">
-        {draftPick.player &&
-          "   " +
-            draftPick.player.firstName +
-            " " +
-            draftPick.player.lastName +
-            " - " +
-            draftPick.player.position}
+        {draftPick.player && (
+          <span>
+            {draftPick.player.firstName +
+              " " +
+              draftPick.player.lastName +
+              " - " +
+              draftPick.player.position}
+          </span>
+        )}
       </div>
     </div>
   );
