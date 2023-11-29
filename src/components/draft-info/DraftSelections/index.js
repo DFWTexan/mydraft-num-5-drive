2c39921c926c29aff176f04dc455c69481791c25
DFWTexan <ErishMF@Gmail.com>
2023-11-28 20:14:21 -0600

    Update index.js

--- src/components/draft-info/DraftSelections/index.js
+++ src/components/draft-info/DraftSelections/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { ListItemButton, ListSubheader, List } from "@mui/material";
 
@@ -14,6 +14,21 @@ const DraftSelections = (props) => {
     isOpen: false,
     player_ID: null,
   });
+
+  // Create a sorted array with the current pick at the top
+  // const sortedDraftPicks = useMemo(() => {
+  //   const currentPickIndex = props.draftPicks.findIndex(
+  //     (pick) => pick.overallPick === draftStatus.currentPick
+  //   );
+  //   if (currentPickIndex === -1) return props.draftPicks; // Return original array if current pick not found
+
+  //   const currentPick = props.draftPicks[currentPickIndex];
+  //   return [
+  //     currentPick,
+  //     ...props.draftPicks.slice(0, currentPickIndex),
+  //     ...props.draftPicks.slice(currentPickIndex + 1),
+  //   ];
+  // }, [props.draftPicks, draftStatus.currentPick]);
 
   const handlePlayerSelected_OpenModal = (player) => {
     setOpenModalPlayerSelected({
