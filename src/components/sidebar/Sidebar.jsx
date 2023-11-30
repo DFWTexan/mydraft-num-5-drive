55b9ed5526ebfb7a47d45b3275eca8657645d386
DFWTexan <ErishMF@Gmail.com>
2023-11-30 16:39:11 -0600

    -  Progress SAVE (Dashboard Icon)
    - Changed Icon for Dashboard Item...

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -156,7 +156,7 @@ const Sidebar = () => {
                 activeIndex === index ? "active" : ""
               }`}
             >
-              <div className="sidebar__menu__item__icon">{item.icon}</div>
+              <span class="material-symbols-outlined dashboard-icon">space_dashboard</span>
               <div className="sidebar__menu__item__text">{item.display}</div>
             </div>
           </Link>
