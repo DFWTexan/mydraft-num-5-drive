1ff627c84963fb8dbe1c70a69917748f7c20405e
DFWTexan <ErishMF@Gmail.com>
2023-12-13 13:27:21 -0600

    -  Progress SAVE (Player News)
    - Completed Player News

--- src/components/draft-list/playerCardItem.js
+++ src/components/draft-list/playerCardItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import "../../styles/index.scss";
-import Placeholder from "../../static/img/no-image.png";  
+import Placeholder from "../../static/img/no-image.png";
+import PlayerNews from "../../static/img/breaking_news.svg";
 
 const getColorClass = (position) => {
   switch (position) {
@@ -23,50 +24,67 @@ const getColorClass = (position) => {
 };
 
 const CardInfo = ({ player }) => (
-  <div className="draft-selection-card__title">
+  <div className="draft-player-list-card__title">
     <div style={{ margin: "1rem" }}>
       <img
-        className="draft-selection-card__image"
+        className="draft-player-list-card__image"
         src={player.photoURL ? player.photoURL : Placeholder}
         alt=""
       />
     </div>
-    <div className="draft-selection-card__player-info">
-      <div className="draft-selection-card__name">
-        {player.firstName} {player.lastName}
+    <div className="draft-player-list-card__player-info">
+      <div className="draft-player-list-card__name">
+        {player.fullName} 
       </div>
-      <div className="draft-selection-card__team">{player.teamAbbr}</div>
+      <div className="draft-player-list-card__team">{player.teamAbbr}</div>
     </div>
-    <div className="draft-selection-card__rankings">
-      {
-        <div
-          className={`draft-selection-card__player-position ${getColorClass(player.position)}`}
+    <div className="draft-player-list-card">
+      <div className="draft-player-list-card__player-details">
+        {/* {player.isPlayerNews ? (
+          <img
+            className="draft-player-list-card__news-icon"
+            src={PlayerNews}
+            alt=""
+          />
+        ) : null} */}
+        <div className={`draft-player-list-card__player-position ${getColorClass(
+            player.position
+          )}`}
         >
+          {player.isPlayerNews ? (
+          <img
+            className="draft-player-list-card__news-icon"
+            src={PlayerNews}
+            alt=""
+          />
+        ) : <div/>}
           {player.position}
         </div>
-      }
-      <table className="draft-selection-card__rankings-table">
-        <thead>
-          <tr>
-            <th className="draft-selection-card__rankings-header">PTS</th>
-            <th className="draft-selection-card__rankings-header">AAV</th>
-            <th className="draft-selection-card__rankings-header">ADP</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="draft-selection-card__rankings-value">
-              {player.pointsVal}
-            </td>
-            <td className="draft-selection-card__rankings-value">
-              {player.aavPoints}
-            </td>
-            <td className="draft-selection-card__rankings-value">
-              {player.adpPoints}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      </div>
+      <div>
+        <table className="draft-player-list-card__rankings-table">
+          <thead>
+            <tr>
+              <th className="draft-player-list-card__rankings-header">PTS</th>
+              <th className="draft-player-list-card__rankings-header">AAV</th>
+              <th className="draft-player-list-card__rankings-header">ADP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="draft-player-list-card__rankings-value">
+                {player.pointsVal}
+              </td>
+              <td className="draft-player-list-card__rankings-value">
+                {player.aavPoints}
+              </td>
+              <td className="draft-player-list-card__rankings-value">
+                {player.adpPoints}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 );
