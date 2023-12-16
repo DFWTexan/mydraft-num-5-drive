2cbae35b0304e7bb57ad6f3acb515686f2ecdeb0
DFWTexan <ErishMF@Gmail.com>
2023-12-16 00:22:51 -0600

    - Cleanup...

--- src/slices/draft.js
+++ src/slices/draft.js
@@ -3,14 +3,6 @@ import DraftDataService from "../services/DraftService";
 
 const initialState = [];
 
-// export const fetchDraftStatus = createAsyncThunk(
-//   "Draft/GetDraftStatus",
-//   async (data) => {
-//     const res = await DraftDataService.getDraftStatus(data);
-//     return res.data;
-//   }
-// );
-
 export const fetchDraftedPlayers = createAsyncThunk(
   "Draft/GetDraftPicksForLeague",
   async (data) => {
@@ -23,29 +15,9 @@ const draftSlice = createSlice({
   name: "draftPicks",
   initialState,
   extraReducers: {
-    // [fetchDraftStatus.fulfilled]: (state, action) => {
-    //   return [...action.payload];
-    // },
     [fetchDraftedPlayers.fulfilled]: (state, action) => {
       return [...action.payload];
     },
-    // [updateTutorial.fulfilled]: (state, action) => {
-    //   const index = state.findIndex(tutorial => tutorial.id === action.payload.id);
-    //   state[index] = {
-    //     ...state[index],
-    //     ...action.payload,
-    //   };
-    // },
-    // [deleteTutorial.fulfilled]: (state, action) => {
-    //   let index = state.findIndex(({ id }) => id === action.payload.id);
-    //   state.splice(index, 1);
-    // },
-    // [deleteAllTutorials.fulfilled]: (state, action) => {
-    //   return [];
-    // },
-    // [findTutorialsByTitle.fulfilled]: (state, action) => {
-    //   return [...action.payload];
-    // },
   },
 });
 
--- src/slices/league.js
+++ src/slices/league.js
@@ -15,29 +15,9 @@ const leagueSlice = createSlice({
   name: "league",
   initialState,
   extraReducers: {
-    // [createTutorial.fulfilled]: (state, action) => {
-    //   state.push(action.payload);
-    // },
     [fetchActiveLeague.fulfilled]: (state, action) => {
       return {...action.payload};
     },
-    // [updateTutorial.fulfilled]: (state, action) => {
-    //   const index = state.findIndex(tutorial => tutorial.id === action.payload.id);
-    //   state[index] = {
-    //     ...state[index],
-    //     ...action.payload,
-    //   };
-    // },
-    // [deleteTutorial.fulfilled]: (state, action) => {
-    //   let index = state.findIndex(({ id }) => id === action.payload.id);
-    //   state.splice(index, 1);
-    // },
-    // [deleteAllTutorials.fulfilled]: (state, action) => {
-    //   return [];
-    // },
-    // [findTutorialsByTitle.fulfilled]: (state, action) => {
-    //   return [...action.payload];
-    // },
   },
 });
 
